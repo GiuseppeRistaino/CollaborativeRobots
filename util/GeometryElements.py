bf4f80dc7d5f7bcf29ff1ea74d130449db911745
Giuseppe <giuseppe.ristaino@gmail.com>
2017-11-18 12:27:28 +0100

    update comment

--- util/GeometryElements.py
+++ util/GeometryElements.py
@@ -1,6 +1,13 @@
 import sympy
 import numpy
 
+'''
+classe che tiene traccia delle informazioni riguardo alla tangente
+x1, y1: coordinate del punto di partenza della retta
+x2, y2: coordinate del punto di intersezione della tangente con la circonferenza
+slope: coefficiente angolare della retta tangente
+obstacle: oggetto di tipo Obstacle a cui è tangente la retta
+'''
 class Tangent:
     def __init__(self, x1=None, y1=None, x2=None, y2=None, slape=None, obstacle=None):
         self.x1 = x1
@@ -11,9 +18,3 @@ class Tangent:
         self.p2 = sympy.Point(x2, y2)
         self.slape = slape
         self.obstacle = obstacle
-
-    def same_slape(self, m):
-        if str(m) != 'oo' and str(self.slape) != 'oo':
-            if sympy.N(self.slape, 1) == sympy.N(m, 1):
-                return True
-        return False
--- util/Search.py
+++ util/Search.py
@@ -1,30 +1,45 @@
 import heapq
 import sympy
 
-
+'''
+classe Nodo utile per tenere traccia delle informazioni riguardanti un nodo del grafo.
+pointSympy: classe Point della libreria sympy
+'''
 class Node():
     def __init__(self, pointSympy):
         self.point = pointSympy
 
+    #funzione per confrontare due oggitti di tipo Nodo.
     def __eq__(self, other):
         return self
 
+    #funzione per confrontare due oggitti di tipo Nodo.
     def __lt__(self, other):
         return self
 
 
-
+'''
+classe utile alla costruzione del grafo.
+edges: dizionario che fa corrispondere a ciascun nodo (id) un dizionario nel quale ciascun nodo corrisponde un arco (cost).
+'''
 class SimpleGraph:
     def __init__(self):
         self.edges = {}
 
+    #funzione che restituisce i nodi adiacenti al nodo (id)
     def neighbors(self, id):
         return self.edges[id]
 
+    #funzione che restituisce il costo dell'arco a partire dal nodo (from_node) ad arrivare al nodo (to_node)
     def cost(self, from_node, to_node):
         return self.edges[from_node][to_node]
 
-
+'''
+classe per la creazione di una coda a priorità utilizzando la classe heapq.
+Oridna gli elementi al suo interno utilizzando una priorità crescente.
+Nel caso in cui gli elementi mostrano una prorità uguale vengono utilizzate le funzioni __eq__ ed __lt__ per
+determinare le loro posizioni nella coda.
+'''
 class PriorityQueue:
     def __init__(self):
         self.elements = []
@@ -38,14 +53,24 @@ class PriorityQueue:
     def get(self):
         return heapq.heappop(self.elements)[1]
 
+'''
+funzione per l'implementazione dell'algoritmo a-star.
+utilizzando una coda a priorità viene determinato il percorso minimo con il rispettivo costo complessivo per attraversarlo,
+partendo dal nodo start e arrivando al nodo goal di un dato grafo.
+graph: oggetto SimpleGraph
+start: sympy.Point
+goal: sympy.Point
 
+N.B.: la coda a priorità deve contenere oggetti di tipo Nodo per la possibilità di poterli confrontare nel caso in cui
+abbiano la stessa priorità.
+'''
 def a_star_search(graph, start, goal):
     frontier = PriorityQueue()
     frontier.put(Node(start), 0)
     came_from = {}  # determina una coppia di nodi dove l'uno è la partenza e l'altro è l'arrivo
     cost_so_far = {}  # determina una coppia: nodo e il costo per arrivarci
-    came_from[start] = None
-    cost_so_far[start] = 0
+    came_from[start] = None #si inizia dal nodo start
+    cost_so_far[start] = 0  #per raggiungere il nodo start ho speso un costo 0
 
     while not frontier.empty():
         current = frontier.get()
@@ -55,16 +80,19 @@ def a_star_search(graph, start, goal):
 
         for next in graph.neighbors(current.point):
             new_cost = cost_so_far[current.point] + graph.cost(current.point, next)
+            #se il costo appena calcolato per arrivare al next è minore rispetto a quello calcolato in precedenza allora aggiorna cost_so_far
             if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[
-                    next] = new_cost  # mi ritrovo nella lista anche un elemento che non potrebbe far parte del path finale (infatti nella ricerca si controllano sempre tutti gli archi e quindi i nodi raggiungibili
+                cost_so_far[next] = new_cost  # mi ritrovo nella lista anche un elemento che non potrebbe far parte del path finale (infatti nella ricerca si controllano sempre tutti gli archi e quindi i nodi raggiungibili
                 priority = new_cost
                 frontier.put(Node(next), priority)
                 came_from[next] = current.point
 
     return came_from, cost_so_far
 
-
+'''
+funzione per la costruzione del percorso a partire dal dizionario came_from.
+RETURN: lsita di punti che fanno parte del path
+'''
 def reconstruct_path(came_from, start, goal):
     current = goal
     path = [current]
@@ -73,4 +101,3 @@ def reconstruct_path(came_from, start, goal):
         path.append(current)
     path.reverse()
     return path
-
